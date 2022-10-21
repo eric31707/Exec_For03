816a4f81beb0efa77e1b27a3dfc5441a63f3978c
ispan <ispan@DESKTOP-PAKSETB>
2022-10-21 12:54:58 +0800

    加入專案檔案。

---
+++ Exec_For03/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_For03
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string snum = "123";
+			//string pnum=snum.PadLeft(6, '0');
+			//string fnum = string.Format("{0:000000}", Convert.ToInt32(snum));
+			//Console.WriteLine("原是字串"+snum+"\n透過padleft:"+pnum+"\n透過String.format:"+fnum);
+
+			for (int length = 1; length <= 10; length++)
+			{
+				Console.WriteLine(new string('*', length).PadLeft(10, ' '));
+			}
+		}
+	}
+}
